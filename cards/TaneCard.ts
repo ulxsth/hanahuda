c6b58e1a9b544beafde0e03bf3dcd3933fba9f46
ulxsth <ulxsth@gmail.com>
2023-12-28 16:49:25 +0900

    タネ札を実装

---
+++ cards/TaneCard.ts
@@ -0,0 +1,24 @@
+import Card from "./Card";
+
+class TaneCard extends Card {
+  constructor(month: number) {
+    let points = 20;
+    super(month, points);
+  }
+
+  show(): void {
+    console.log(`タネ札: ${this.month}月`);
+  }
+}
+
+class InoShikaChoCard extends TaneCard {
+  constructor(month: number) {
+    super(month);
+  }
+}
+
+class SakeCard extends TaneCard {
+  constructor(month: number) {
+    super(month);
+  }
+}
