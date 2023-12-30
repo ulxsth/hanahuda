f866c21536afd75e4e695b1c8428410b5ffbfa98
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-30 09:49:17 +0900

    u: 小野道風にshowを実装

--- src/entity/cards/HikariCard.ts
+++ src/entity/cards/HikariCard.ts
@@ -12,11 +12,19 @@ export class HikariCard extends Card {
   equals(card: Card): boolean {
     return card instanceof HikariCard && this.month === card.month;
   }
+
+  toString(): string {
+    return `hikari-${this.month}`;
+  }
 }
 
 export class OnoDofuCard extends HikariCard {
   constructor(month: number) {
     super(month);
+  }
+
+  show(): void {
+    console.log(`光札: ${this.month}月 (小野道風)`);
   }
 }
 
