01197d7014b397c86510faf7e79eb989f5f2dbc0
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-30 09:18:03 +0900

    from,push, concat,filterを実装

---
+++ src/entity/Hand.ts
@@ -0,0 +1,39 @@
+import Card from './cards/Card';
+
+export class Hand {
+  private _cards: Card[];
+
+  constructor() {
+    this._cards = [];
+  }
+
+  get length() {
+    return this._cards.length;
+  }
+
+  get cards() {
+    return this._cards;
+  }
+
+  public from(cards: Card[]): void {
+    this._cards = cards;
+  }
+
+  public push(card: Card): void {
+    this._cards.push(card);
+  }
+
+  public concat(cards: Card[]): Hand {
+    const arr = this._cards.concat(cards);
+    const hand = new Hand();
+    hand.from(arr);
+    return hand;
+  }
+
+  public filter(callbackfn: (value: Card, index: number, array: Card[]) => unknown): Hand {
+    const arr = this._cards.filter(callbackfn);
+    const hand = new Hand();
+    hand.from(arr);
+    return hand;
+  }
+}
